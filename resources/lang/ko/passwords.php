b2bc1441242bac3d40d3698f37212be33fca373b
DDinghoya <ddinghoya@gmail.com>
2022-12-22 19:52:05 +0900

    Create passwords.php

---
+++ resources/lang/ko/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '비밀번호가 초기화되었습니다!',
+    'sent' => '비밀번호 재설정 링크를 이메일로 보냈습니다!',
+    'throttled' => '다시 시도하기 전에 잠시 기다려 주세요.',
+    'token' => '이 비밀번호 재설정 토큰은 유효하지 않습니다.',
+    'user' => "해당 이메일 주소를 가진 사용자를 찾을 수 없습니다.",
+
+];
